4b1c5eb1c69ac1fd4e13559d631d3c0147910402
Hrishabh Raj <hrishabh.raj4705@gmail.com>
2021-08-30 20:28:46 +0530

    Added hillclimb

---
+++ Assignment-2 Hill Climbing and Simulated Annealing/README.md
@@ -0,0 +1,49 @@
+
+# CS561 - ARTIFICIAL INTELLIGENCE LAB  
+## ASSIGNMENT-1: A* Search, BFS  
+
+### Group Information  
+----------------------
+|HEADER|INFORMATION|
+|------|-----------|
+|GroupID | 1801cs12_1801cs16_1801cs22 | 
+|Date | 25/08/2021  |
+|1801CS12 | Bablu Kumar  |
+|1801CS16 | Mangesh Chandrawanshi|  
+|1801CS22 | Hrishabh Raj  |
+  
+### Contents 
+
+	main.py : driver code
+	puzzle.py : puzzle Class, heuristic functions
+	Utils.py : complementory functions for input/output
+	Astar.py : Astar algorithm Class
+	BFS.py : BFS algorithm Class
+	StartState : Input file for start state
+	GoalState : Input file for goal state
+
+### Usage 
+
+Sample Input :
+
+	StartState 
+	T8 T3 T5 
+	T4 T1 T6 
+	T2 T7 B
+
+	GoalState
+	T1 T2 T3 
+	T8 B T4 
+	T7 T6 T5
+
+Note: Avoid extra whitespaces/newlines while providing input throught these files
+
+### Execution :
+
+	python main.py 
+
+Sample Output :
+
+	Refer to test1.txt in Tests directory
+
+Thank You
---
+++ Assignment-2 Hill Climbing and Simulated Annealing/SimulatedAnnealing.py
@@ -0,0 +1,94 @@
+# Simulated Annealing Algorithm
+
+# Import Required Functionalities 
+from puzzle import Puzzle, heuristicFuncList
+from heapq import heappush, heappop
+from time import time
+
+class SimulatedAnnealing:
+	# Cost Function = h(n)
+	def __init__(self,heuristic,heuristicFunc,puzzle):
+		self.heuristic = heuristic
+		self.heuristicFunc = heuristicFunc
+		self.puzzle = puzzle
+		self.distance = {}
+		self.exploredStates = 0
+
+	class Node:
+		# value is h(n) as per problem statement, will be used to compare nodes
+		# puzzle is the puzzle configuration
+		# parent is the parent node, used to find the path
+		def __init__(self, value, puzzle, parent=None):
+			self.puzzle = puzzle
+			self.parent = parent
+			self.value = value
+
+		def __lt__(self, other):
+			return self.value < other.value
+
+		def findNeighbours(self):
+			blankX, blankY = Puzzle.findCell(self.puzzle.puzzleConfig, 0)
+			swap_pos = [(-1,0),(1,0),(0,1),(0,-1)]
+
+			neighbours = []
+			for dx, dy in swap_pos:
+				newX = dx + blankX
+				newY = dy + blankY
+
+				if(newX < 0 or newY < 0 or newX == 3 or newY == 3):
+					continue
+
+				newPuzzle = currNode.puzzle.newConfig(blankX,blankY,newX,newY)
+				neighbours.append(newPuzzle)
+
+			return neighbours
+
+
+
+	def run(self):
+
+		start_time = time()
+		h_n = self.heuristicFunc(self.puzzle)
+		self.distance[self.puzzle] = 0
+		currNode = self.Node(h_n, self.puzzle)
+
+		while currNode:			
+			# Goal State Reached
+			if currNode.puzzle.isGoal():
+				break
+
+			# Increment count of Explored States
+			self.exploredStates += 1
+			g_n = self.distance[currNode.puzzle]
+
+			#explore adjacent nodes
+			neighbours = currNode.findNeighbours()
+
+			bestNode = None
+			for newPuzzle in neighbours:
+				if newPuzzle in self.distance:
+					continue
+				h_n = self.heuristicFunc(newPuzzle)
+				newNode = self.Node(h_n, newPuzzle, currNode)
+
+				if bestNode and newNode <= bestNode:
+					bestNode = newNode
+
+				elif not bestNode and newNode <= currNode:
+					bestNode = newNode
+
+			currNode = bestNode
+			if currNode:
+				self.distance[currNode.puzzle] = g_n + 1
+
+
+		# Store path from START_STATE to GOAL_STATE if exists
+		path = []
+		while currNode:
+			path.append(currNode.puzzle)
+			currNode = currNode.parent
+
+		# Calculate execution time
+		exec_time = time()-start_time
+
+		return exec_time, path
