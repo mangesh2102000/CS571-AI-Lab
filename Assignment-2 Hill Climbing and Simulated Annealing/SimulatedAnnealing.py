e8744ae88156e33a1a9af2de73aab71092b70442
Hrishabh Raj <hrishabh.raj4705@gmail.com>
2021-08-31 11:24:48 +0530

    added cooling and probfunc

--- Assignment-2 Hill Climbing and Simulated Annealing/SimulatedAnnealing.py
+++ Assignment-2 Hill Climbing and Simulated Annealing/SimulatedAnnealing.py
@@ -4,14 +4,22 @@
 from puzzle import Puzzle, heuristicFuncList
 from heapq import heappush, heappop
 from time import time
+import random
+from math import exp
+
+def coolingFunc(temperature):
+	return 0.9*temperature
+
+def calcProbability(diff, temperature):
+	return exp(-diff/temperature)
 
 class SimulatedAnnealing:
 	# Cost Function = h(n)
-	def __init__(self,heuristic,heuristicFunc,puzzle):
+	def __init__(self,heuristic,heuristicFunc,puzzle,temperature):
 		self.heuristic = heuristic
 		self.heuristicFunc = heuristicFunc
 		self.puzzle = puzzle
-		self.distance = {}
+		self.maxTemperature = temperature
 		self.exploredStates = 0
 
 	class Node:
@@ -25,6 +33,9 @@ class SimulatedAnnealing:
 
 		def __lt__(self, other):
 			return self.value < other.value
+
+		def __le__(self, other):
+			return self.value <= other.value
 
 		def findNeighbours(self):
 			blankX, blankY = Puzzle.findCell(self.puzzle.puzzleConfig, 0)
@@ -46,47 +57,45 @@ class SimulatedAnnealing:
 
 
 	def run(self):
+		self.exploredStates = 0
 
 		start_time = time()
 		h_n = self.heuristicFunc(self.puzzle)
-		self.distance[self.puzzle] = 0
+		currTemperature = self.maxTemperature
+		finalTemperature = 0.1
+		distance[self.puzzle] = 0
 		currNode = self.Node(h_n, self.puzzle)
 
-		while currNode:			
+		while currTemprature > finalTemperature:			
 			# Goal State Reached
 			if currNode.puzzle.isGoal():
 				break
 
 			# Increment count of Explored States
 			self.exploredStates += 1
-			g_n = self.distance[currNode.puzzle]
 
 			#explore adjacent nodes
 			neighbours = currNode.findNeighbours()
 
-			bestNode = None
-			for newPuzzle in neighbours:
-				if newPuzzle in self.distance:
-					continue
-				h_n = self.heuristicFunc(newPuzzle)
-				newNode = self.Node(h_n, newPuzzle, currNode)
+			newPuzzle = random.choice(neighbours)
+			h_n = self.heuristicFunc(self.puzzle)
+			newNode = self.Node(h_n, newPuzzle, currNode)
 
-				if bestNode and newNode <= bestNode:
-					bestNode = newNode
+			if newNode <= currNode:
+				currNode = newNode
 
-				elif not bestNode and newNode <= currNode:
-					bestNode = newNode
-
-			currNode = bestNode
-			if currNode:
-				self.distance[currNode.puzzle] = g_n + 1
+			else:
+				probability = calcProbability(newNode.value-currNode.value, currTemprature)
+				if random.uniform(0,1) <= probability:
+					currNode = newNode
 
 
 		# Store path from START_STATE to GOAL_STATE if exists
 		path = []
-		while currNode:
-			path.append(currNode.puzzle)
-			currNode = currNode.parent
+		if currNode.puzzle.isGoal():
+			while currNode:
+				path.append(currNode.puzzle)
+				currNode = currNode.parent
 
 		# Calculate execution time
 		exec_time = time()-start_time
