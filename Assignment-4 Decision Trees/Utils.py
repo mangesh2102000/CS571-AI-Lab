a75fbb633127a793683a6cb32142680ba3f9d2f1
hrisbh10 <hrishabh.raj4705@gmail.com>
2021-10-05 01:16:50 +0530

    assign4 code done

---
+++ Assignment-4 Decision Trees/Utils.py
@@ -0,0 +1,72 @@
+from nltk import ngrams, pos_tag
+from nltk.corpus import stopwords
+from string import punctuation
+
+stop_words = set(stopwords.words('english'))
+
+def getMean(values):
+	s = 0
+	for x in values:
+		s += x
+	s /= len(values)
+	return s
+
+def processInput(filepath):
+	def getPosTag(line):
+		words = [word for word in line if word not in stop_words]
+		tagged = pos_tag(words)
+		return tagged
+
+	file = open(filepath)
+
+	dataset = []
+	Unigrams = []
+	Bigrams = []
+	Trigrams = []
+	posTagged = []
+	avgLength = 0
+
+	for line in file:
+		sample = line.split()
+		classname = sample[0].split(':')[0]
+		question = ' '.join(sample[1:]).translate(str.maketrans('','',punctuation)).rstrip()
+
+		sample = question.split()
+
+		length = len(sample)
+		unigrams = list(ngrams(sample,1))
+		bigrams = list(ngrams(sample,2))
+		trigrams = list(ngrams(sample,3))
+		posTag = getPosTag(sample)
+
+		row = [classname, question, length, unigrams, bigrams, trigrams, posTag]
+
+		dataset.append(row)
+		avgLength += length
+		Unigrams.extend(unigrams)
+		Bigrams.extend(bigrams)
+		Trigrams.extend(trigrams)
+		posTagged.extend(posTag)
+
+	avgLength /= len(dataset)
+
+	return dataset, avgLength, Unigrams, Bigrams, Trigrams, posTagged
+
+class Question:
+	Header = ['Class','Text','Length','Unigrams','Bigrams','Trigrams','POS_Tag']
+	
+	def __init__(self, colId, value):
+		self.colId = colId
+		self.value = value
+
+	def match(self, row):
+		val = row[self.colId]
+
+		if self.colId == 2:
+			return val >= self.value
+
+		return self.value in val
+
+	def __str__(self):
+		return f"Does {Question.Header[self.colId]} contains {self.value}?"
+		
---
+++ Assignment-4 Decision Trees/main.py
@@ -0,0 +1,160 @@
+from Utils import processInput, Question, getMean
+from DecisionTree import DecisionTree
+from collections import Counter
+from sklearn.model_selection import KFold
+from sklearn.metrics import f1_score, precision_score, recall_score, accuracy_score, confusion_matrix
+
+
+dataset, avgLength, unigrams, bigrams, trigrams, postags = processInput("traindata.txt")
+
+labels = [row[0] for row in dataset]
+labels = list(set(labels))
+print("Labels for prediction:", labels)
+
+
+# Extract top ngrams
+
+# 500 top 1-gram
+unigram_count = Counter(unigrams).most_common(500)
+# 300 top 2-gram
+bigram_count = Counter(bigrams).most_common(300)
+# 200 top 3-gram
+trigram_count = Counter(trigrams).most_common(200)
+# 500 top POS_Tags
+tags_count = Counter(postags).most_common(500)
+
+def prepareQuestions(uniFlag=True, biFlag=True, triFlag=True, posFlag=True, lenFlag=True):
+	questions = []
+
+	if lenFlag:
+		questions.append(Question(2,avgLength))
+
+	if uniFlag:
+		for gram in unigram_count:
+			questions.append(Question(3,gram[0]))
+
+	if biFlag:
+		for gram in bigram_count:
+			questions.append(Question(4,gram[0]))
+
+	if triFlag:
+		for gram in trigram_count:
+			questions.append(Question(5,gram[0]))
+
+	if posFlag:
+		for gram in tags_count:
+			questions.append(Question(6,gram[0]))
+
+	return questions
+
+allQuestions = prepareQuestions()
+
+def getPrediction(classifier, test_data):
+	predicted_values = []
+	for row in test_data:
+		prediction = classifier.classify(row)
+		predicted_values.append(prediction)
+
+	return predicted_values
+
+
+def get10FoldCVResult():
+	print("Preparing 10-fold cross-validation results...")
+	kf = KFold(10, shuffle=True, random_state=1)
+
+	precision = []
+	f_score = []
+	recall = []
+	for trainIds, testIds in kf.split(dataset):
+		train_data = []
+		test_data = []
+		for x in trainIds:
+			train_data.append(dataset[x])
+
+		print("Training...")
+		dTreeCf = DecisionTree(train_data,allQuestions,"gini")
+
+		actual_values = []
+
+		for x in testIds:
+			test_data.append(dataset[x])
+
+			actual = dataset[x][0]
+			actual_values.append(actual)
+
+		predicted_values = getPrediction(dTreeCf, test_data)
+
+		precision.append(precision_score(actual_values, predicted_values, average='macro'))
+		recall.append(recall_score(actual_values, predicted_values, average='macro'))
+		f_score.append(f1_score(actual_values, predicted_values, average='macro'))
+		
+	print("Avg. Precision:", getMean(precision))
+	print("Avg. Recall:", getMean(recall))
+	print("Avg. F-Score:", getMean(f_score))
+
+
+# get10FoldCVResult()
+
+testData, tavgLen, tUnigrams, tBigrams, tTrigrams, tPostags = processInput("testdata.txt")
+
+def featureAblationReport():
+	print("Feature ablation report(avgLength, unigrams, bigrams, trigrams, postags): ")
+	features = ["Unigrams","Bigrams","Trigrams","POS_Tags","AvgLength"]
+
+	for case in range(1<<5):
+		params = [True]*5
+		for t in range(5):
+			if (case & (1<<t)):
+				continue
+			else:
+				params[t] = False
+
+		#prepareQuestions with True params only.
+		print("\n\nTraining DecisionTree with ",end="")
+		for t in range(5):
+			if params[t]:
+				print(features[t],end="; ")
+			else:
+				print("No",features[t],end="; ")
+		print("\n---------------------------------------------------------------")
+
+		dTreeCf = DecisionTree(dataset, 
+					prepareQuestions(params[0],params[1],params[2],params[3],params[4]),
+					"gini")
+
+		predicted_values = getPrediction(dTreeCf, testData)
+		actual_values = [row[0] for row in testData]
+
+		matrix = confusion_matrix(actual_values, predicted_values, labels=labels)
+		acc = matrix.diagonal()/matrix.sum(axis=1)
+
+		accuracy_report = dict(zip(labels, acc))
+
+		print("Accuracy Report:", accuracy_report)
+
+# featureAblationReport()
+
+criteria = ['entropy','gini','miserror']
+
+def getModelReport(criteria):
+	print(f"\n\nTraining model with {criteria} information gain...")
+	dTreeCf = DecisionTree(dataset,allQuestions,criteria)
+	predicted_values = getPrediction(dTreeCf, testData)
+	actual_values = [row[0] for row in testData]
+
+	matrix = confusion_matrix(actual_values, predicted_values, labels=labels)
+
+	precision = precision_score(actual_values, predicted_values, average='macro')
+	recall = recall_score(actual_values, predicted_values, average='macro')
+	f_score = f1_score(actual_values, predicted_values, average='macro')
+
+	return precision, recall, f_score, matrix
+
+for c in criteria:
+	precision, recall, f_score, matrix = getModelReport(c)
+	print(f"'{c}' Model Report")
+	print("----------------------------")
+	print("Precision:", precision)
+	print("Recall:", recall)
+	print("F-Score:", f_score)
+	print("Confusion Matrix:\n", matrix)
